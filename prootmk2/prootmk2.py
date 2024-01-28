3976423bdac453bbafec521a710439110d01c5bd
Ha2k4r <155183725+Ha2k4r@users.noreply.github.com>
2024-01-27 20:16:20 -0500

    Update prootmk2.py
    Whoops fixed indent size

--- prootmk2/prootmk2.py
+++ prootmk2/prootmk2.py
@@ -24,7 +24,8 @@ I=0
 def settings(pixel_mapper_config):
     options = RGBMatrixOptions()
     options.brightness = 100
-    options.rows = 32     options.hardware_mapping='adafruit-hat-pwm'
+    options.rows = 32     
+  options.hardware_mapping='adafruit-hat-pwm'
     options.cols = 64
     options.scan_mode = 1
     options.show_refresh_rate = True
