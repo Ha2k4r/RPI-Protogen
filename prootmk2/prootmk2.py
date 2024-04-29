9c62cfb24022224312939bb791192a16569997b9
Ha2k4r <155183725+Ha2k4r@users.noreply.github.com>
2024-04-28 23:01:49 -0400

    Debug Mode Update
    Added debug mode
    added canvasing
    made new lists (future expansion is a definite) 
    completely overhauled previous Update() design 
    reduced settings bulk and unnecessary bloat

--- prootmk2/prootmk2.py
+++ prootmk2/prootmk2.py
@@ -1,11 +1,11 @@
 import ExpressionVectors as EXV
-
 import PathCalculator as PathC
 import time
+from PIL import Image
 import numpy as np
 import cv2
-from Options import *
 
+from Options import *
 try:
     from rgbmatrix import RGBMatrix, RGBMatrixOptions
 except ImportError:
@@ -13,8 +13,6 @@ except ImportError:
         exit(ImportError("ImportError: Failed to import RGB Matrix Library"))
     else:
         pass
-
-
 # Declarations ignore
 StartTimeN = 0
 StartTime = 0
@@ -22,51 +20,43 @@ StartTimeF = 0
 StartTimeBS = time.time() + BlinkEvery
 StartTimeM = 0
 IsBlink = False
-
-
 TRUEBLANK = np.zeros([32, 64], dtype='uint8')
 EmptyCanvas = np.zeros([32, 64], dtype='uint8')
+RIGHT_MAIN_MATRIX="RightMatrix"
+LEFT_MAIN_MATRIX="LeftMatrix"
 
-def Display(Matrix, preprocessed_image):
-    image = Image.fromarray(cv2.cvtColor(preprocessed_image, cv2.COLOR_BGR2RGB))
-    image.thumbnail((Matrix.width, Matrix.height), Image.LANCZOS)
-    Matrix.SetImage(image)
-
-def settings(pixel_mapper_config, Brightness):
-    if (pixel_mapper_config == True):
-            options = RGBMatrixOptions()
-            options.drop_privileges= Drop_Privs
-            options.brightness = Brightness
-            options.rows = 32
-            options.hardware_mapping='adafruit-hat-pwm'
-            options.cols = 64
-            options.scan_mode = 1
-            options.gpio_slowdown = 3
-            options.chain_length = 2
-            options.pwm_lsb_nanoseconds = Global_Nano_Seconds
-            options.limit_refresh_rate_hz = Refresh_Hertz_Cap
-            options.parallel = 1
-            return RGBMatrix(options=options)
-    else:
-            options = RGBMatrixOptions()
-            options.drop_privileges= Drop_Privs
-            options.brightness = Brightness
-            options.rows = 32
-            options.hardware_mapping='adafruit-hat-pwm'
-            options.cols = 64
-            options.scan_mode = 1
-            options.gpio_slowdown = 3
-            options.chain_length = 2
-            options.pwm_lsb_nanoseconds = Global_Nano_Seconds
-            options.limit_refresh_rate_hz = Refresh_Hertz_Cap
-            options.parallel = 1
-            options.pixel_mapper_config = "Mirror:H"
-            return RGBMatrix(options=options)
+def Display(Screen, image):
+    
+    image = Image.fromarray(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+    image.thumbnail((Screens[Screen].width, Screens[Screen].height), Image.LANCZOS)
+    Buffer[Screen].SetImage(image.convert("RGB"))
+    Buffer[Screen] = Screens[Screen].SwapOnVSync(Buffer[Screen])
+    
+def Settings(Flip):
+    # Configuration for the matrix
+    options = RGBMatrixOptions()
+    options.cols = 64
+    options.chain_length = 2
+    options.drop_privileges=False
+    options.hardware_mapping = 'adafruit-hat-pwm'  # If you have an Adafruit HAT: 'adafruit-hat'
+    options.gpio_slowdown = 4
+    options.pwm_lsb_nanoseconds = 50
+    if Flip:
+        options.pixel_mapper_config = "Mirror:H"
+    
+    return RGBMatrix(options = options)
 def BlinkUpdate(UndecodedArray,Index):
     DecodedArray = PathC.Make_Geometric_Figure(Index, UndecodedArray)
     return DecodedArray
-
 EyePathArray = []
+Screens = {
+    RIGHT_MAIN_MATRIX: Settings(False),
+    LEFT_MAIN_MATRIX: Settings(True)
+}
+Buffer = {
+    RIGHT_MAIN_MATRIX: Screens[RIGHT_MAIN_MATRIX].CreateFrameCanvas(),
+    LEFT_MAIN_MATRIX: Screens[LEFT_MAIN_MATRIX].CreateFrameCanvas()
+}
 while True:
     ElapsedTimeF = time.time() - StartTimeF
     if ElapsedTimeF >= GradientScrollSpeed:
@@ -79,13 +69,9 @@ while True:
                 MovingColorMap.set(cv2.CAP_PROP_POS_FRAMES, 0)
         else:
             StartTimeF = time.time()
-
-
-
     ElapsedTime = time.time() - StartTime
     ElapsedTimeBS = time.time() - StartTimeBS
     cycles = 50
-
 #Eyes Time Check
     if IsBlink:
         if ElapsedTimeBS >= BlinkSpeed:  # skips the wait to continue the animation
@@ -101,7 +87,6 @@ while True:
                 IsBlink = False
                 StartTime = time.time()
                 continue
-
             EyeChords = BlinkUpdate(EyePathArray, Index)
             EyeVector = cv2.fillPoly(EmptyCanvas, [EyeChords], 255, 0, 0)  # updates sprite for RightMatrix
             EmptyCanvas = np.zeros_like(TRUEBLANK)
@@ -130,23 +115,17 @@ while True:
         EmptyCanvas = np.zeros_like(TRUEBLANK)
         StartTimeN = time.time()
         changed = True
-
     #Update screens
     if changed and not DebugMode:
         mask = EyeVector + MouthVector + NoseVector
         masked = cv2.bitwise_and(frame, frame, mask=mask)
-        LeftMatrix = settings(True, Brightness)
-        RightMatrix = settings(False, Brightness)
-        
         changed = False
-
-        Display(RightMatrix, masked)
-        Display(LeftMatrix, masked)
-
+        Display(RIGHT_MAIN_MATRIX, masked)
+        Display(LEFT_MAIN_MATRIX, masked)
+    
     elif changed and DebugMode:
         mask = EyeVector + MouthVector + NoseVector
         masked = cv2.bitwise_and(frame, frame, mask=mask)
-
         # Update screen
         masked = cv2.resize(masked, (1200, 600))
         cv2.imshow("image", masked)
