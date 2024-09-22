dbc4b57548ca8d225187ec4fc72ae8464d6878db
Ha2k4r <155183725+Ha2k4r@users.noreply.github.com>
2024-09-22 19:32:48 -0400

    Serial update 4 / 10
    color correction gpio slowdown and some new option handling which is nice

--- prootmk3/Matrix_Drw.cpp
+++ prootmk3/Matrix_Drw.cpp
@@ -29,17 +29,22 @@ void CopyImageToCanvas(const cv::Mat &image, Canvas *canvas) {
 }
 
 RGBMatrix* InitializeMatrix() {
-  RGBMatrix::Options matrix_options;
-  matrix_options.pwm_lsb_nanoseconds = 60;
-  //matrix_options.limit_refresh_rate_hz = 100;
-  //matrix_options.disable_hardware_pulsing = false;
-  matrix_options.rows = 32;
-  matrix_options.cols = 64;
-  matrix_options.chain_length = 2;
-  matrix_options.parallel = 1;
-  matrix_options.hardware_mapping = "adafruit-hat-pwm";
 
-  RGBMatrix* matrix = RGBMatrix::CreateFromOptions(matrix_options, rgb_matrix::RuntimeOptions());
+  rgb_matrix::RuntimeOptions runtime_opt;
+  RGBMatrix::Options options;
+
+  options.pwm_lsb_nanoseconds = 60;
+  //options.show_refresh_rate = true;
+  //options.limit_refresh_rate_hz = 100;
+  //options.disable_hardware_pulsing = false;
+  options.rows = 32;
+  options.cols = 64;
+  options.chain_length = 2;
+  options.hardware_mapping = "adafruit-hat-pwm";
+
+  runtime_opt.gpio_slowdown = 4;
+
+  RGBMatrix* matrix = RGBMatrix::CreateFromOptions(options, runtime_opt);
   if (matrix == NULL) {
     fprintf(stderr, "Failed to initialize matrix.\n");
   }
@@ -52,8 +57,8 @@ void DisplayImage(RGBMatrix* matrix, const cv::Mat &image) {
     fprintf(stderr, "Failed to load image.\n");
     return;
   }
-
   
   CopyImageToCanvas(image, offscreen_canvas);
   offscreen_canvas = matrix->SwapOnVSync(offscreen_canvas);
+  offscreen_canvas -> Clear();
 }
