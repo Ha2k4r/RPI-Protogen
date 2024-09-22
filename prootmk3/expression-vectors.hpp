bbff0a446f5da3a7aab7921754ddcb9215c63720
Ha2k4r <155183725+Ha2k4r@users.noreply.github.com>
2024-09-22 19:37:02 -0400

    Serial update 7 / 10
    added some cleanup declarations to better make main look nicer be prepared for more of this in future refactor updates

---
+++ prootmk3/expression-vectors.hpp
@@ -0,0 +1,11 @@
+#ifndef EXPR_V_H
+#define EXPR_V_H
+#include <opencv2/opencv.hpp>
+extern cv::Point EyeHappyArray[9];
+extern cv::Point EyeClosedHappyArray[9];
+extern cv::Point MouthHappyArray[14];
+extern cv::Point NoseHappyArray[10];
+extern cv::Point BlushStripe1[2];
+extern cv::Point BlushStripe2[2];
+extern cv::Point BlushStripe3[2];
+#endif
