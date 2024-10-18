05810aa6b1c00610fbbf229934bf2860fdaf319e
Ha2k4r <155183725+Ha2k4r@users.noreply.github.com>
2024-10-18 14:50:36 -0400

    Removal of passed lengths in Path Calculator
    made it so that no value is required to be passed into it and defined beforehand and now all the size calculation should happen within the functions themselves

--- prootmk3/PathCalculator.hpp
+++ prootmk3/PathCalculator.hpp
@@ -1,13 +1,21 @@
-#ifndef PATHCALCULATOR_H
-#define PATHCALCULATOR_H
+#ifndef SPRITE_MATH_H
+#define SPRITE_MATH_H
 
-#include <math.h>
+#include <boost/asio.hpp>
 #include <opencv2/opencv.hpp>
+#include "PathCalculator.hpp"
+#include "globals.hpp"
+class SpriteMath {
+public:
+    void EyeUpdate();
 
-int Calculate_Single_Bezier_Curve(double t, int p0, int p1, int p2, int p3);
+    void MouthUpdate();
 
-std::vector<cv::Point> Calculate_Many_Bezier_Curves(const std::vector<cv::Point>& Array,const std::vector<cv::Point>& Target_Array, int num_points,int arraysize, double factor1 = 0.5 , double factor2 = 1.8 );
+    void NoseUpdate();
 
-std::vector<cv::Point> UnpackBezierArray(int Index, std::vector<cv::Point> RawBezierArray, int numpoints, int maxindex);
+protected:
+    std::vector<cv::Point> EyeVerticies;
+    std::vector<cv::Point> RawBezierEye;
+};
 
 #endif
