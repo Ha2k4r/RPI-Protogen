db2eff0d40c84791fb4049ad810e2c344f625fee
Ha2k4r <enigma558@proton.me>
2025-05-28 18:10:34 -0400

    Desktop Testbench support cleaning excess files

---
+++ prootmk3/Config.hpp
@@ -0,0 +1,7 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+//Change these values to dramatically change the function of the protogen
+
+#define TESTING_ENVIRONMENT //Is this on a desktop computer for testing or is this on a raspberry pi that you actually wanna see the RGB matrix
+
+#endif
